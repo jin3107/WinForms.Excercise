6238d6ba4ebeb990300ee5040d5fa78e9e746808
Chuong Huynh <tanchuonghuynh3@gmail.com>
2025-03-20 21:43:57 +0700

    Bai 10 trang 39

---
+++ 023101063-Tuan6/023101063-Tuan6/Hinh2-27.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace _023101063_Tuan6
+{
+    partial class Hinh2_27
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblDSXe = new System.Windows.Forms.Label();
+            this.lblTTCT = new System.Windows.Forms.Label();
+            this.tvwDSXe = new System.Windows.Forms.TreeView();
+            this.lvwTTCT = new System.Windows.Forms.ListView();
+            this.colHang = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.colNamSX = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.colDongCo = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.colGia = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // lblDSXe
+            // 
+            this.lblDSXe.AutoSize = true;
+            this.lblDSXe.Location = new System.Drawing.Point(12, 9);
+            this.lblDSXe.Name = "lblDSXe";
+            this.lblDSXe.Size = new System.Drawing.Size(125, 22);
+            this.lblDSXe.TabIndex = 0;
+            this.lblDSXe.Text = "Danh sách xe:";
+            // 
+            // lblTTCT
+            // 
+            this.lblTTCT.AutoSize = true;
+            this.lblTTCT.Location = new System.Drawing.Point(310, 9);
+            this.lblTTCT.Name = "lblTTCT";
+            this.lblTTCT.Size = new System.Drawing.Size(148, 22);
+            this.lblTTCT.TabIndex = 1;
+            this.lblTTCT.Text = "Thông tin chi tiết:";
+            // 
+            // tvwDSXe
+            // 
+            this.tvwDSXe.Location = new System.Drawing.Point(16, 34);
+            this.tvwDSXe.Name = "tvwDSXe";
+            this.tvwDSXe.Size = new System.Drawing.Size(273, 218);
+            this.tvwDSXe.TabIndex = 2;
+            this.tvwDSXe.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.tvwDSXe_AfterSelect);
+            // 
+            // lvwTTCT
+            // 
+            this.lvwTTCT.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.colHang,
+            this.colNamSX,
+            this.colDongCo,
+            this.colGia});
+            this.lvwTTCT.FullRowSelect = true;
+            this.lvwTTCT.GridLines = true;
+            this.lvwTTCT.HideSelection = false;
+            this.lvwTTCT.Location = new System.Drawing.Point(314, 34);
+            this.lvwTTCT.Name = "lvwTTCT";
+            this.lvwTTCT.Size = new System.Drawing.Size(323, 218);
+            this.lvwTTCT.TabIndex = 3;
+            this.lvwTTCT.UseCompatibleStateImageBehavior = false;
+            this.lvwTTCT.View = System.Windows.Forms.View.Details;
+            // 
+            // colHang
+            // 
+            this.colHang.Text = "Hãng";
+            // 
+            // colNamSX
+            // 
+            this.colNamSX.Text = "Năm SX";
+            // 
+            // colDongCo
+            // 
+            this.colDongCo.Text = "Động cơ";
+            // 
+            // colGia
+            // 
+            this.colGia.Text = "Giá";
+            // 
+            // Hinh2_27
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 22F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(650, 266);
+            this.Controls.Add(this.lvwTTCT);
+            this.Controls.Add(this.tvwDSXe);
+            this.Controls.Add(this.lblTTCT);
+            this.Controls.Add(this.lblDSXe);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Hinh2_27";
+            this.Text = "Hinh2_27";
+            this.Load += new System.EventHandler(this.Hinh2_27_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblDSXe;
+        private System.Windows.Forms.Label lblTTCT;
+        private System.Windows.Forms.TreeView tvwDSXe;
+        private System.Windows.Forms.ListView lvwTTCT;
+        private System.Windows.Forms.ColumnHeader colHang;
+        private System.Windows.Forms.ColumnHeader colNamSX;
+        private System.Windows.Forms.ColumnHeader colDongCo;
+        private System.Windows.Forms.ColumnHeader colGia;
+    }
+}
---
+++ 023101063-Tuan6/023101063-Tuan6/Hinh2-27.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _023101063_Tuan6
+{
+    public partial class Hinh2_27 : Form
+    {
+        public Hinh2_27()
+        {
+            InitializeComponent();
+        }
+
+        private void Hinh2_27_Load(object sender, EventArgs e)
+        {
+            tvwDSXe.Nodes.Add("Xe");
+            tvwDSXe.Nodes[0].Nodes.Add("Xe máy");
+            tvwDSXe.Nodes[0].Nodes[0].Nodes.Add("SH");
+            tvwDSXe.Nodes[0].Nodes[0].Nodes.Add("Spacy");
+
+            tvwDSXe.Nodes[0].Nodes.Add("Ô tô");
+            tvwDSXe.Nodes[0].Nodes[1].Nodes.Add("Innova");
+            tvwDSXe.Nodes[0].Nodes[1].Nodes.Add("Camry");
+        }
+
+        private void tvwDSXe_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            string seletedIndex = tvwDSXe.SelectedNode.Text;
+            if (seletedIndex == "Xe máy")
+                HienThiXeMay();
+            if (seletedIndex == "Ô tô")
+                HienThiOto();
+        }
+
+        private void HienThiOto()
+        {
+            lvwTTCT.Items.Clear();
+            lvwTTCT.Items.Add("Innova");
+            lvwTTCT.Items[0].SubItems.Add("2014");
+            lvwTTCT.Items[0].SubItems.Add("150");
+            lvwTTCT.Items[0].SubItems.Add("52000000");
+            
+            lvwTTCT.Items.Add("Camry");
+            lvwTTCT.Items[1].SubItems.Add("2013");
+            lvwTTCT.Items[1].SubItems.Add("100");
+            lvwTTCT.Items[1].SubItems.Add("150000000");
+        }
+
+        private void HienThiXeMay()
+        {
+            lvwTTCT.Items.Clear();
+            lvwTTCT.Items.Add("SH");
+            lvwTTCT.Items[0].SubItems.Add("2012");
+            lvwTTCT.Items[0].SubItems.Add("125");
+            lvwTTCT.Items[0].SubItems.Add("40000000");
+            
+            lvwTTCT.Items.Add("Spacy");
+            lvwTTCT.Items[1].SubItems.Add("2013");
+            lvwTTCT.Items[1].SubItems.Add("125");
+            lvwTTCT.Items[1].SubItems.Add("42000000");
+        }
+    }
+}
