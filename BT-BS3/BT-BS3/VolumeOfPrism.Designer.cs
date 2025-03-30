5817e92a75e5dad48bda7d1b9f55e28db7efd1cd
Chuong Huynh <tanchuonghuynh3@gmail.com>
2025-03-30 21:07:36 +0700

    BT-BS3

---
+++ BT-BS3/BT-BS3/VolumeOfPrism.Designer.cs
@@ -0,0 +1,299 @@
+﻿namespace BT_BS3
+{
+    partial class VolumeOfPrism
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTTLT = new System.Windows.Forms.Label();
+            this.lblNhap = new System.Windows.Forms.Label();
+            this.lblDiemA = new System.Windows.Forms.Label();
+            this.lblDiemB = new System.Windows.Forms.Label();
+            this.lvlDiemC = new System.Windows.Forms.Label();
+            this.lblDiemD = new System.Windows.Forms.Label();
+            this.lblChieuCao = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblDoDaiAB = new System.Windows.Forms.Label();
+            this.lblDoDaiBC = new System.Windows.Forms.Label();
+            this.lblDoDaiAC = new System.Windows.Forms.Label();
+            this.txtX_A = new System.Windows.Forms.TextBox();
+            this.txtY_A = new System.Windows.Forms.TextBox();
+            this.txtY_B = new System.Windows.Forms.TextBox();
+            this.txtX_B = new System.Windows.Forms.TextBox();
+            this.txtY_C = new System.Windows.Forms.TextBox();
+            this.txtX_C = new System.Windows.Forms.TextBox();
+            this.txtY_D = new System.Windows.Forms.TextBox();
+            this.txtX_D = new System.Windows.Forms.TextBox();
+            this.txtChieuCaoLT = new System.Windows.Forms.TextBox();
+            this.btNhapDiem = new System.Windows.Forms.Button();
+            this.btDoDaiDoan = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblTTLT
+            // 
+            this.lblTTLT.AutoSize = true;
+            this.lblTTLT.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTTLT.ForeColor = System.Drawing.Color.Red;
+            this.lblTTLT.Location = new System.Drawing.Point(223, 9);
+            this.lblTTLT.Name = "lblTTLT";
+            this.lblTTLT.Size = new System.Drawing.Size(267, 29);
+            this.lblTTLT.TabIndex = 0;
+            this.lblTTLT.Text = "THỂ TÍCH LĂNG TRỤ";
+            // 
+            // lblNhap
+            // 
+            this.lblNhap.AutoSize = true;
+            this.lblNhap.Location = new System.Drawing.Point(25, 59);
+            this.lblNhap.Name = "lblNhap";
+            this.lblNhap.Size = new System.Drawing.Size(181, 22);
+            this.lblNhap.TabIndex = 1;
+            this.lblNhap.Text = "Nhập 4 điểm A,B,C,D";
+            // 
+            // lblDiemA
+            // 
+            this.lblDiemA.AutoSize = true;
+            this.lblDiemA.Location = new System.Drawing.Point(25, 136);
+            this.lblDiemA.Name = "lblDiemA";
+            this.lblDiemA.Size = new System.Drawing.Size(22, 22);
+            this.lblDiemA.TabIndex = 2;
+            this.lblDiemA.Text = "A";
+            // 
+            // lblDiemB
+            // 
+            this.lblDiemB.AutoSize = true;
+            this.lblDiemB.Location = new System.Drawing.Point(139, 136);
+            this.lblDiemB.Name = "lblDiemB";
+            this.lblDiemB.Size = new System.Drawing.Size(22, 22);
+            this.lblDiemB.TabIndex = 3;
+            this.lblDiemB.Text = "B";
+            // 
+            // lvlDiemC
+            // 
+            this.lvlDiemC.AutoSize = true;
+            this.lvlDiemC.Location = new System.Drawing.Point(251, 136);
+            this.lvlDiemC.Name = "lvlDiemC";
+            this.lvlDiemC.Size = new System.Drawing.Size(23, 22);
+            this.lvlDiemC.TabIndex = 4;
+            this.lvlDiemC.Text = "C";
+            // 
+            // lblDiemD
+            // 
+            this.lblDiemD.AutoSize = true;
+            this.lblDiemD.Location = new System.Drawing.Point(363, 136);
+            this.lblDiemD.Name = "lblDiemD";
+            this.lblDiemD.Size = new System.Drawing.Size(23, 22);
+            this.lblDiemD.TabIndex = 5;
+            this.lblDiemD.Text = "D";
+            // 
+            // lblChieuCao
+            // 
+            this.lblChieuCao.AutoSize = true;
+            this.lblChieuCao.Location = new System.Drawing.Point(25, 230);
+            this.lblChieuCao.Name = "lblChieuCao";
+            this.lblChieuCao.Size = new System.Drawing.Size(224, 22);
+            this.lblChieuCao.TabIndex = 6;
+            this.lblChieuCao.Text = "Chiều cao lăng trụ tứ giác: ";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 284);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(203, 22);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Thể tích của lăng trụ là: ";
+            // 
+            // lblDoDaiAB
+            // 
+            this.lblDoDaiAB.AutoSize = true;
+            this.lblDoDaiAB.Location = new System.Drawing.Point(388, 230);
+            this.lblDoDaiAB.Name = "lblDoDaiAB";
+            this.lblDoDaiAB.Size = new System.Drawing.Size(141, 22);
+            this.lblDoDaiAB.TabIndex = 8;
+            this.lblDoDaiAB.Text = "Độ dài đoạn AB:";
+            // 
+            // lblDoDaiBC
+            // 
+            this.lblDoDaiBC.AutoSize = true;
+            this.lblDoDaiBC.Location = new System.Drawing.Point(388, 284);
+            this.lblDoDaiBC.Name = "lblDoDaiBC";
+            this.lblDoDaiBC.Size = new System.Drawing.Size(142, 22);
+            this.lblDoDaiBC.TabIndex = 9;
+            this.lblDoDaiBC.Text = "Độ dài đoạn BC:";
+            // 
+            // lblDoDaiAC
+            // 
+            this.lblDoDaiAC.AutoSize = true;
+            this.lblDoDaiAC.Location = new System.Drawing.Point(388, 337);
+            this.lblDoDaiAC.Name = "lblDoDaiAC";
+            this.lblDoDaiAC.Size = new System.Drawing.Size(142, 22);
+            this.lblDoDaiAC.TabIndex = 10;
+            this.lblDoDaiAC.Text = "Độ dài đoạn AC:";
+            // 
+            // txtX_A
+            // 
+            this.txtX_A.Location = new System.Drawing.Point(62, 120);
+            this.txtX_A.Name = "txtX_A";
+            this.txtX_A.Size = new System.Drawing.Size(46, 28);
+            this.txtX_A.TabIndex = 11;
+            // 
+            // txtY_A
+            // 
+            this.txtY_A.Location = new System.Drawing.Point(62, 154);
+            this.txtY_A.Name = "txtY_A";
+            this.txtY_A.Size = new System.Drawing.Size(46, 28);
+            this.txtY_A.TabIndex = 12;
+            // 
+            // txtY_B
+            // 
+            this.txtY_B.Location = new System.Drawing.Point(172, 154);
+            this.txtY_B.Name = "txtY_B";
+            this.txtY_B.Size = new System.Drawing.Size(46, 28);
+            this.txtY_B.TabIndex = 14;
+            // 
+            // txtX_B
+            // 
+            this.txtX_B.Location = new System.Drawing.Point(172, 120);
+            this.txtX_B.Name = "txtX_B";
+            this.txtX_B.Size = new System.Drawing.Size(46, 28);
+            this.txtX_B.TabIndex = 13;
+            // 
+            // txtY_C
+            // 
+            this.txtY_C.Location = new System.Drawing.Point(283, 154);
+            this.txtY_C.Name = "txtY_C";
+            this.txtY_C.Size = new System.Drawing.Size(46, 28);
+            this.txtY_C.TabIndex = 16;
+            // 
+            // txtX_C
+            // 
+            this.txtX_C.Location = new System.Drawing.Point(283, 120);
+            this.txtX_C.Name = "txtX_C";
+            this.txtX_C.Size = new System.Drawing.Size(46, 28);
+            this.txtX_C.TabIndex = 15;
+            // 
+            // txtY_D
+            // 
+            this.txtY_D.Location = new System.Drawing.Point(392, 154);
+            this.txtY_D.Name = "txtY_D";
+            this.txtY_D.Size = new System.Drawing.Size(46, 28);
+            this.txtY_D.TabIndex = 18;
+            // 
+            // txtX_D
+            // 
+            this.txtX_D.Location = new System.Drawing.Point(392, 120);
+            this.txtX_D.Name = "txtX_D";
+            this.txtX_D.Size = new System.Drawing.Size(46, 28);
+            this.txtX_D.TabIndex = 17;
+            // 
+            // txtChieuCaoLT
+            // 
+            this.txtChieuCaoLT.Location = new System.Drawing.Point(255, 224);
+            this.txtChieuCaoLT.Name = "txtChieuCaoLT";
+            this.txtChieuCaoLT.Size = new System.Drawing.Size(100, 28);
+            this.txtChieuCaoLT.TabIndex = 19;
+            // 
+            // btNhapDiem
+            // 
+            this.btNhapDiem.Location = new System.Drawing.Point(538, 110);
+            this.btNhapDiem.Name = "btNhapDiem";
+            this.btNhapDiem.Size = new System.Drawing.Size(126, 38);
+            this.btNhapDiem.TabIndex = 20;
+            this.btNhapDiem.Text = "Nhập điểm";
+            this.btNhapDiem.UseVisualStyleBackColor = true;
+            // 
+            // btDoDaiDoan
+            // 
+            this.btDoDaiDoan.Location = new System.Drawing.Point(538, 154);
+            this.btDoDaiDoan.Name = "btDoDaiDoan";
+            this.btDoDaiDoan.Size = new System.Drawing.Size(126, 38);
+            this.btDoDaiDoan.TabIndex = 21;
+            this.btDoDaiDoan.Text = "Độ dài đoạn";
+            this.btDoDaiDoan.UseVisualStyleBackColor = true;
+            // 
+            // VolumeOfPrism
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 22F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(684, 440);
+            this.Controls.Add(this.btDoDaiDoan);
+            this.Controls.Add(this.btNhapDiem);
+            this.Controls.Add(this.txtChieuCaoLT);
+            this.Controls.Add(this.txtY_D);
+            this.Controls.Add(this.txtX_D);
+            this.Controls.Add(this.txtY_C);
+            this.Controls.Add(this.txtX_C);
+            this.Controls.Add(this.txtY_B);
+            this.Controls.Add(this.txtX_B);
+            this.Controls.Add(this.txtY_A);
+            this.Controls.Add(this.txtX_A);
+            this.Controls.Add(this.lblDoDaiAC);
+            this.Controls.Add(this.lblDoDaiBC);
+            this.Controls.Add(this.lblDoDaiAB);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblChieuCao);
+            this.Controls.Add(this.lblDiemD);
+            this.Controls.Add(this.lvlDiemC);
+            this.Controls.Add(this.lblDiemB);
+            this.Controls.Add(this.lblDiemA);
+            this.Controls.Add(this.lblNhap);
+            this.Controls.Add(this.lblTTLT);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "VolumeOfPrism";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "VolumeOfPrism";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTTLT;
+        private System.Windows.Forms.Label lblNhap;
+        private System.Windows.Forms.Label lblDiemA;
+        private System.Windows.Forms.Label lblDiemB;
+        private System.Windows.Forms.Label lvlDiemC;
+        private System.Windows.Forms.Label lblDiemD;
+        private System.Windows.Forms.Label lblChieuCao;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblDoDaiAB;
+        private System.Windows.Forms.Label lblDoDaiBC;
+        private System.Windows.Forms.Label lblDoDaiAC;
+        private System.Windows.Forms.TextBox txtX_A;
+        private System.Windows.Forms.TextBox txtY_A;
+        private System.Windows.Forms.TextBox txtY_B;
+        private System.Windows.Forms.TextBox txtX_B;
+        private System.Windows.Forms.TextBox txtY_C;
+        private System.Windows.Forms.TextBox txtX_C;
+        private System.Windows.Forms.TextBox txtY_D;
+        private System.Windows.Forms.TextBox txtX_D;
+        private System.Windows.Forms.TextBox txtChieuCaoLT;
+        private System.Windows.Forms.Button btNhapDiem;
+        private System.Windows.Forms.Button btDoDaiDoan;
+    }
+}
---
+++ BT-BS3/BT-BS3/VolumeOfPrism.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BT_BS3
+{
+    public partial class VolumeOfPrism : Form
+    {
+        public VolumeOfPrism()
+        {
+            InitializeComponent();
+        }
+    }
+}
