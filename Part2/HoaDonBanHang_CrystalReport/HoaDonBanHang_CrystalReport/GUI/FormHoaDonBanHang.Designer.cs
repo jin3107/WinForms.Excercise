be81d4f71cb84335e852db609c531643bc8b6140
Rainy <tanchuonghuynh3@gmail.com>
2025-11-17 11:59:50 +0700

    Crystal report Hoa don ban hang

---
+++ Part2/HoaDonBanHang_CrystalReport/HoaDonBanHang_CrystalReport/GUI/FormHoaDonBanHang.Designer.cs
@@ -0,0 +1,65 @@
+﻿namespace HoaDonBanHang_CrystalReport.GUI
+{
+    partial class FormHoaDonBanHang
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.crystalReportHoaDon = new CrystalDecisions.Windows.Forms.CrystalReportViewer();
+            this.SuspendLayout();
+            // 
+            // crystalReportHoaDon
+            // 
+            this.crystalReportHoaDon.ActiveViewIndex = -1;
+            this.crystalReportHoaDon.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.crystalReportHoaDon.Cursor = System.Windows.Forms.Cursors.Default;
+            this.crystalReportHoaDon.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.crystalReportHoaDon.Location = new System.Drawing.Point(0, 0);
+            this.crystalReportHoaDon.Name = "crystalReportHoaDon";
+            this.crystalReportHoaDon.Size = new System.Drawing.Size(1008, 589);
+            this.crystalReportHoaDon.TabIndex = 0;
+            // 
+            // FormHoaDonBanHang
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 22F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1008, 589);
+            this.Controls.Add(this.crystalReportHoaDon);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "FormHoaDonBanHang";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "FormHoaDonBanHang";
+            this.Load += new System.EventHandler(this.FormHoaDonBanHang_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private CrystalDecisions.Windows.Forms.CrystalReportViewer crystalReportHoaDon;
+    }
+}
---
+++ Part2/HoaDonBanHang_CrystalReport/HoaDonBanHang_CrystalReport/GUI/FormHoaDonBanHang.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using HoaDonBanHang_CrystalReport.DAO;
+
+namespace HoaDonBanHang_CrystalReport.GUI
+{
+    public partial class FormHoaDonBanHang : Form
+    {
+        Database db;
+        DataTable dt;
+
+        public FormHoaDonBanHang()
+        {
+            InitializeComponent();
+        }
+
+        private void FormHoaDonBanHang_Load(object sender, EventArgs e)
+        {
+            db = new Database("localhost", "HoaDonBanHang");
+            string sql = "Select H.SOHD, NGHD, N.HOTEN as HOTENNV, K.HOTEN as HOTENKH, C.MASP, TENSP, SL, S.TRIGIA " +
+                "From HOADON H, NHANVIEN N, KHACHHANG K, CTHD C, SANPHAM S " +
+                "Where H.MANV = N.MANV and H.MAKH = K.MAKH and C.SOHD = H.SOHD and C.MASP = S.MASP";
+            dt = db.ExcuteQuery(sql);
+            CrystalReportHoaDonBanHang cry = new CrystalReportHoaDonBanHang();
+            cry.SetDataSource(dt);
+            crystalReportHoaDon.ReportSource = cry;
+        }
+    }
+}
