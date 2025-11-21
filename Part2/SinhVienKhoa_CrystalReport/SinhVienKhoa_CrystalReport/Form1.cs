df2d895df85ab4a6d00a3e5ff8a9de4ccbf0c75f
Rainy <tanchuonghuynh3@gmail.com>
2025-11-21 14:58:48 +0700

    init

---
+++ Part2/SinhVienKhoa_CrystalReport/SinhVienKhoa_CrystalReport/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SinhVienKhoa_CrystalReport
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Database db = new Database("localhost", "QLSV1");
+            string sql = "Select k.MAKHOA, TENKHOA, MASV, HOSV + ' ' + TENSV as HOTEN, NGAYSINH, NOISINH, HOCBONG " +
+                "From DMKHOA k, DMSV s " +
+                "Where k.MAKHOA = s.MAKH";
+            DataTable dt = db.ExcuteQuery(sql);
+            CrystalReportSinhVien cry = new CrystalReportSinhVien();
+            cry.SetDataSource(dt);
+            crystalReportViewer1.ReportSource = cry;
+        }
+    }
+}
---
+++ Part2/SinhVienKhoa_CrystalReport/SinhVienKhoa_CrystalReport/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("SinhVienKhoa_CrystalReport")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SinhVienKhoa_CrystalReport")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("b20bc046-8e33-4761-9af1-1067c37911db")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
